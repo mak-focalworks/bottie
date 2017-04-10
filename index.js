342709aacf46afaac5095a20c9f318e9233233c9
VTHEVMU <muthukumar.thevar@pearson.com>
2017-04-10 19:13:50 +0530

    Added start script, added default get for index.js

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ var Bottie = {
 app.get('/',function(req,res){
   res.end("<h1>It Works</h1>");
 });
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+});
 var customPhrasesText;
 var customPhrases;
 try {
